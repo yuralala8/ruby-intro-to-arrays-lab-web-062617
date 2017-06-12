248998384764c3b939a9d5ce5a55cfd875cdbf0a
yuralala8 <yc1435@nyu.edu>
2017-06-12 13:46:43 -0400

    arrays commit

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+    my_new_array = []
+end
+
+def array_with_two_elements
+    my_new_array = ["elem1", "elem2"]
+end
+
+def first_element(my_first_element)
+    my_first_element[0]
+end
+
+def third_element(my_third_element)
+    my_third_element[2]
+end
+
+def last_element(my_last_element)
+    my_last_element[-1]
+end
+
+def first_element_with_array_methods(first_country)
+    first_country.first
+end
+
+def last_element_with_array_methods(last_country)
+    last_country.last
+end
+
+def length_of_array(length)
+    length.length
+end
